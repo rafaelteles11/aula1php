80268dd82dcb4e6eda82cc5c10e11b4fe942264b
rafaelteles11 <rafaelocteles@gmail.com>
2024-09-12 11:45:11 -0300

    Add files via upload

---
+++ login/default.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang= "en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, inital-scale-1.0">
+        <title>Login</title>
+</head>
+<body>
+    <?php
+    echo "Primeiro PHP!<br>";
+    $nome = "Rafael";
+    $sobrenome = "Teles";
+    echo $nome . " " . $sobrenome;
+    ?>
+    </body>
+    </html>
